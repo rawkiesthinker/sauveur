cadec665942e7eeaebf7619a3450e9f2d8b4b730
rawkthethinker <47385943+rawkthethinker@users.noreply.github.com>
2020-03-03 15:44:23 +0200

    it is time to create the commmnity that will make a dent in the world

--- src/App.js
+++ src/App.js
@@ -3,9 +3,23 @@ import './App.css';
 
 function App() {
   return (
-    <div className="">
-      
+
+    <div className="w-full h-screen ">
+      <div className="w-full h-12 flex p-2">
+        <div className="w-1/2 flex">
+        <h2 className="font-deafult-bold uppercase text-2xl">Sauveur</h2>
+        </div>
+        <div className="w-1/2 flex justify-end">
+        <p className="font-default">Community of freedom seekers</p>
+        </div>
+       
+      </div>
+      <div className="w-full h-screen justify-center items-center flex">
+        <div className="font-default-black uppercase opacity-75 text-4xl">Hello world</div>
+      </div>
+
     </div>
+
   );
 }
 
